771791a07f9db0191f9ebf9a12c54cfd8c2be22e
JediPythonCode <157297468+JediPythonCode@users.noreply.github.com>
2026-07-25 08:36:52 -0600

    Update dashboard.py
    Signed-off-by: JediPythonCode <157297468+JediPythonCode@users.noreply.github.com>

--- dashboard.py
+++ dashboard.py
@@ -242,7 +242,7 @@ st.markdown(
 st.markdown(
     """
     <div class="hero-container">
-        <div class="hero-title">Utah Real Estate. <br> Investments.</div>
+        <div class="hero-title">Utah Real Estate. <br> Utah Wholesale Houses & Real Estate For Sale.</div>
         <div class="hero-subtitle">Utah Private Real Estate Opportunities. Wholesale Utah Real Estate Contracts For Sale. </div>
     </div>
     <div id="contracts-section"></div>
